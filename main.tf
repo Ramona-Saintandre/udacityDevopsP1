110a67109089d03aeb29b7821bf5d2d5d378c5d2
ramona-Saintandre <codinggrandma@gmail.com>
2021-02-21 15:14:26 -0500

    fixed line  45 and 75

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "azurerm_network_security_group" "UdacityP1" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
 
   # Create virtual network
   resource "azurerm_virtual_network" "UdacityP1" {
@@ -69,13 +70,14 @@ resource "azurerm_network_security_group" "UdacityP1" {
     resource_group_name     = azurerm_resource_group.UdacityP1.name
     allocation_method       = "Static"
     idle_timeout_in_minutes = 30
-
+  
     tags = {
       environment = "UdacityP1"
     }
-  }
+    }
+  
 
-}
+
 
 # VM security rules
 resource "azurerm_network_security_rule" "UdacityP1vm" {
@@ -120,13 +122,14 @@ resource "azurerm_lb" "UdacityP1" {
   tags = {
     "environment" = "UdacityP1"
   }
-}
+
 
 resource "azurerm_lb_backend_address_pool" "UdacityP1" {
   name                = "BackendPool"
   resource_group_name = azurerm_resource_group.UdacityP1.name
   loadbalancer_id     = azurerm_lb.UdacityP1.id
 }
+
 resource "azurerm_lb_probe" "UdacityP1" {
   resource_group_name = azurerm_resource_group.UdacityP1.name
   loadbalancer_id     = azurerm_lb.UdacityP1.id
@@ -174,7 +177,7 @@ resource "azurerm_network_interface" "UdacityP1" {
     subnet_id                     = azurerm_subnet.UdacityP1.id
     private_ip_address_allocation = "Dynamic"
   }
-}
+
 # Create linux virtual machine
 resource "azurerm_linux_virtual_machine" "UdacityP1" {
   count               = var.vm_count
@@ -197,7 +200,7 @@ resource "azurerm_linux_virtual_machine" "UdacityP1" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-}
+
 resource "azurerm_managed_disk" "UdacityP1-md" {
   count                = var.vm_count
   name                 = "UdacityP1-md_${count.index}"
@@ -217,19 +220,3 @@ resource "azurerm_virtual_machine_data_disk_attachment" "UdacityP1" {
 }
 
 
-# subscription_id = "1d53902c-4bc6-44c8-82da-d1a59f04c098"
-# client_id = "129af8a1-a479-4e19-8e7a-10a633a172e6"
-# client_secret = "IF79ZP2xDzyR~tbfKEOAFAf72Kdmrsb8Sb"
-# tenant_id = "38e23809-f428-4d08-9f4e-b3b8b2df585c"
-
-# Resource Group/Location
-# location = "eastus"
-# resource_group = "UdacityP1"
-# application_type = "webApp"
-
-# # Network
-# address_space = ["10.5.0.0/16"]
-# address_prefix_test = "10.5.1.0/24"
-
-# packer_image_id =  "/subscriptions/1d53902c-4bc6-44c8-82da-d1a59f04c098/resourceGroups/UdacityP1/providers/Microsoft.Compute/images/PackerServerImage"
-# vm_count = 2
